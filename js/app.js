c7cbc273fd1d2899d1faa3928aef860d5e54750a
Giang Photon <46670524+giangphoton@users.noreply.github.com>
2019-02-19 21:08:35 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,12 +31,6 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
                     <li>Wipe with a soft cloth</li>
                     <li>Made in Vietnam</li>
                 </ul>`,
-              details: [
-                { list: 'Marble' },
-                { list: 'Each will be unique' },
-                { list: 'Wipe with a soft cloth' },
-                { list: 'Made in Vietnam' }
-              ],
               price: 39.95
             },
             {
@@ -51,14 +45,14 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'CV002',
               description: 'Image 10',
               summary: 'Dark Waters. Teardrop-shaped ceramic vase holds fresh greenery and stems in moody blue hues.',
-              moresummary: 'Deep blue reactive glaze washes over top, finishing each piece unique.',
-              details: [
-                { list: 'Overall Dimensions: Height: 8.75 X Diameter: 6' },
-                { list: 'Ceramic with dark blue reactive glaze' },
-                { list: 'Each will be unique' },
-                { list: 'Wipe with a soft cloth' },
-                { list: 'Made in Vietnam' }
-              ],
+              moresummary: `Deep blue reactive glaze washes over top, finishing each piece unique.
+              <ul>
+                  <li>Overall Dimensions: Height: 8.75" X Diameter: 6"</li>
+                  <li>Ceramic with dark blue reactive glaze</li>
+                  <li>Each will be unique</li>
+                  <li>Wipe with a soft cloth</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 29.95
             },
             {
@@ -73,15 +67,15 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'FS003',
               description: 'Image 20',
               summary: 'Blue Room. Clean lines in glossy navy lacquer span almost five feet to broaden storage options.',
-              moresummary: 'Low-profile frame with expansive top can even pedestal a widescreen. Two clean-front doors hide two adjustable A/V-ready shelves (one on each side) with a gap and cutouts for cord management. Engineered wood case floats on slim steel L feet in brushed nickel-plated finish.',
-              details: [
-                { list: 'Low-emission engineered wood with hi-gloss navy finish'},
-                { list: 'Steel L feet with a brushed nickel-plated finish'},
-                { list: 'Top holds TV or stereo up to 50 lbs. evenly distributed'},
-                { list: 'Shelves have built-in gaps for cords; back has cord cutouts'},
-                { list: 'Clean with a soft, dry cloth'},
-                { list: 'Made in Vietnam'}
-              ],
+              moresummary: `Low-profile frame with expansive top can even pedestal a widescreen. Two clean-front doors hide two adjustable A/V-ready shelves (one on each side) with a gap and cutouts for cord management. Engineered wood case floats on slim steel L feet in brushed nickel-plated finish.
+              <ul>
+                  <li>Low-emission engineered wood with hi-gloss navy finish</li>
+                  <li>Steel L feet with a brushed nickel-plated finish</li>
+                  <li>Top holds TV or stereo up to 50 lbs. evenly distributed</li>
+                  <li>Shelves have built-in gaps for cords; back has cord cutouts</li>
+                  <li>Clean with a soft, dry cloth</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 499
             },
             {
@@ -96,14 +90,14 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'TW004',
               description: 'Image 30',
               summary: 'Gold Diggers. Get a hold on gold at the table. Flatware is forged with substantial heft in 18/0 stainless steel (13/0 for the knife) with a gold-colored finish.',
-              moresummary: 'Matte finish handles contrast gleaming polished heads. Five-piece settings dine a party of four.',
-              details: [
-                { list: '18/0 forged stainless steel' },
-                { list: 'Gold finish with matte handles' },
-                { list: 'Hand wash' },
-                { list: 'Hand drying is recommended to prevent discoloration and film build-up' },
-                { list: 'Made in Vietnam' }
-              ],
+              moresummary: `Matte finish handles contrast gleaming polished heads. Five-piece settings dine a party of four.
+              <ul>
+                  <li>18/0 forged stainless steel</li>
+                  <li>Gold finish with matte handles</li>
+                  <li>Hand wash</li>
+                  <li>Hand drying is recommended to prevent discoloration and film build-up</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 89.95
             },
             {
@@ -118,14 +112,14 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'HD005',
               description: 'Image 40',
               summary: 'Earth And Sky. Abstract landscape is crafted from strips of natural water hyacinth.',
-              moresummary: 'Fibers are dyed an earthy spectrum of blue, black, gold and red then folded into triangular shapes and arranged into the conceptual mosaic you see here. We love the texture it adds to any space.',
-              details: [
-                { list: 'Water hyacinth and engineered wood' },
-                { list: 'Each will be unique' },
-                { list: 'Different wall materials may require different types of fasteners; use fasteners suitable for the walls in your home' },
-                { list: 'Dust with soft, dry cloth' },
-                { list: 'Made in Vietnam' }
-              ],
+              moresummary: `Fibers are dyed an earthy spectrum of blue, black, gold and red then folded into triangular shapes and arranged into the conceptual mosaic you see here. We love the texture it adds to any space.
+              <ul>
+                  <li>Water hyacinth and engineered wood</li>
+                  <li>Each will be unique</li>
+                  <li>Different wall materials may require different types of fasteners; use fasteners suitable for the walls in your home</li>
+                  <li>Dust with soft, dry cloth</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 199
             },
             {
@@ -140,14 +134,14 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'CH006',
               description: 'Image 50',
               summary: 'Point Made. Cast aluminum pillar candle holder makes strong sculptural statement in antique brass finish.',
-              moresummary: 'Attention-grabbing from every angle.',
-              details: [
-                { list: 'Cast aluminum with antiqued brass finish' },
-                { list: 'Each will be unique' },
-                { list: 'Max pillar candle height: 6"' },
-                { list: 'Clean with a soft cloth' },
-                { list: 'Made in Vietnam' }
-              ],
+              moresummary: `Attention-grabbing from every angle.
+              <ul>
+                  <li>Cast aluminum with antiqued brass finish</li>
+                  <li>Each will be unique</li>
+                  <li>Max pillar candle height: 6"</li>
+                  <li>Clean with a soft cloth</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 44.95
             },
             {
@@ -162,14 +156,14 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'WD007',
               description: 'Image 60',
               summary: 'Feast Al Fresco. Designer Jannis Ellenberger explores the beauty of a spare, simple form. Defined by planes and angles, the architectural profile is elevated by a the rich tones of 100% acacia wood with a natural oiled finish that will naturally patina and lighten over time.',
-              moresummary: 'Unique angled legs support a slatted surface with built-in umbrella hole and room to seat 4. Makes the perfect dining companion with the Artemis Dining Chairs and Bench.',
-              details: [
-                { list: 'Acacia wood with galvanized steel hardware' },
-                { list: 'Wood will naturally patina and age with time' },
-                { list: 'Outdoor-safe; cover or store indoors during inclement weather and when not in use.' },
-                { list: 'Clean with a soft damp cloth; no abrasive cleaners' },
-                { list: 'Made in Vietnam' }
-              ],
+              moresummary: `Unique angled legs support a slatted surface with built-in umbrella hole and room to seat 4. Makes the perfect dining companion with the Artemis Dining Chairs and Bench.
+              <ul>
+                  <li>Acacia wood with galvanized steel hardware</li>
+                  <li>Wood will naturally patina and age with time</li>
+                  <li>Outdoor-safe; cover or store indoors during inclement weather and when not in use</li>
+                  <li>Clean with a soft damp cloth; no abrasive cleaners</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 399
             },
             {
@@ -184,13 +178,13 @@ var app = angular.module('productApp', ['ngAnimate', 'ngTouch', 'ngCookies', 'sl
               sku: 'GD008',
               description: 'Image 70',
               summary: 'Fresh Start. Decorative pressed-glass appetizer plates dish small bites with vintage attitude.',
-              moresummary: 'Layer with your dinnerware for a fresh take on everyday. Pair with Daphne Low Glass Bowl.',
-              details: [
-                { list: '7.5" dia. x 1"H' },
-                { list: 'Soda lime glass' },
-                { list: 'Dishwasher- and microwave-safe' },
-                { list: 'Made in Vietnam' }
-              ],
+              moresummary: `Layer with your dinnerware for a fresh take on everyday. Pair with Daphne Low Glass Bowl.
+              <ul>
+                  <li>7.5" dia. x 1"H</li>
+                  <li>Soda lime glass</li>
+                  <li>Dishwasher- and microwave-safe</li>
+                  <li>Made in Vietnam</li>
+              </ul>`,
               price: 19.95
             },
         ];
